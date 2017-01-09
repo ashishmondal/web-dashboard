ddf4547936b0c3c2d64abf27369ba65dcffd4d41
Ashish <ashish@mondal.in>
2017-01-09 08:25:02 -0600

    deploy using gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var ftp = require('vinyl-ftp');
+
+var user = process.env.FTP_USER;
+var password = process.env.FTP_PWD;
+
+if(!user || !password){
+    throw 'Username or password not provided!';
+}
+
+console.log(user + ':' + password);
+
+gulp.task('deploy', function() {
+  var remotePath = '/';
+
+  var conn = ftp.create({
+    host: 'ftp.mondal.in',
+    user: user,
+    password: password,
+    log: gutil.log
+  });
+
+  gulp.src(['dist/**'], { base: '.', buffer: false })
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath));
+});
