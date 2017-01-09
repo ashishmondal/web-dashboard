5cdc7ccc1a6f1888032ad16948d5b8686643f6de
Ashish <ashish@mondal.in>
2017-01-09 08:47:33 -0600

    test ftp

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,48 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var ftp = require('vinyl-ftp');
 
+var Client = require('ftp');
+var fs = require('fs');
+
+
 var user = process.env.FTP_USER;
 var password = process.env.FTP_PWD;
 
-if(!user || !password){
+if (!user || !password) {
     throw 'Username or password not provided!';
 }
 
 console.log(user + ':' + password);
 
-gulp.task('deploy', function() {
-  var remotePath = '/';
+gulp.task('deploy', function () {
+    var remotePath = '/';
 
-  var conn = ftp.create({
-    host: 'ftp.mondal.in',
-    user: user,
-    password: password,
-    log: gutil.log
-  });
+    var conn = ftp.create({
+        host: 'ftp.mondal.in',
+        user: user,
+        password: password,
+        log: gutil.log
+    });
 
-  gulp.src(['dist/**'], { base: '.', buffer: false })
-    .pipe(conn.newer(remotePath))
-    .pipe(conn.dest(remotePath));
+    var c = new Client({
+        host: 'ftp.mondal.in',
+        user: user,
+        password: password,
+        log: gutil.log
+    });
+
+    c.on('ready', function () {
+        console.log('ftp: ready');
+        c.put('package.json', function (err) {
+            if (err) throw err;
+            console.log('ftp: put');
+            c.end();
+        });
+    });
+    // connect to localhost:21 as anonymous 
+    c.connect();
+
+    gulp.src(['dist/**'], { base: '.', buffer: false })
+        .pipe(conn.newer(remotePath))
+        .pipe(conn.dest(remotePath));
 });
